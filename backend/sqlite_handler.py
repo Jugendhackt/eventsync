c1daeab564cc959454c4d2fe811b88e9e27a30a8
Merlin <70809264+smyril42@users.noreply.github.com>
2024-10-04 16:54:38 +0200

    create sqlite_handler.py

---
+++ backend/sqlite_handler.py
@@ -0,0 +1,18 @@
+from sqlite3 import connect
+
+
+db_path = 'backend/db/contentdb.db'
+
+
+class SQLiteHandler:
+    def __init__(self, file=db_path):
+        self.file = file
+
+    def __enter__(self):
+        self.conn = connect(self.file)
+        # self.conn.row_factory = sqlite3.Row
+        return self.conn.cursor()
+
+    def __exit__(self, err_type, value, traceback):
+        self.conn.commit()
+        self.conn.close()
