f326349d97cbcfa73203060f36696f85b2ab92bf
EpicLight <163542112+epiclight1400@users.noreply.github.com>
2024-10-04 15:06:29 +0200

    added backend db

---
+++ src/backend/content_db.py
@@ -0,0 +1,38 @@
+import aiosqlite
+import asyncio
+
+db_path = 'src/backend/db/contentdb.db'
+
+class ContentDBInit:
+    def __init__(self):
+        self.db_path = db_path
+
+    async def init_db(self):
+        async with aiosqlite.connect(self.db_path) as db:
+            await db.execute(
+                """
+                CREATE TABLE IF NOT EXISTS content_db (
+                    event_id TEXT,
+                    name TEXT,
+                    author TEXT,
+                    description TEXT,
+                    type INTEGER,
+                    create_time TEXT DEFAULT CURRENT_TIMESTAMP,
+                    delete_after TEXT,
+                    time TEXT,
+                    hrtime TEXT,
+                    location TEXT,
+                    
+                )
+                """
+            )
+            await db.commit()  # Korrekt: await db.commit()
+            print("Datenbank erstellt")
+
+async def main():
+    db_init = ContentDBInit()
+    await db_init.init_db()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
