01f3561ffbb92521f13ae892fec46da6f971c80d
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-05 14:59:06 +0200

    Change to snake case, fix unresolved inputs

--- backend/api.py
+++ backend/api.py
@@ -7,7 +7,7 @@ from uvicorn import run as uvicorn_run
 from sqlite_handler import SQLiteHandler
 from event import Event
 
-from jwt_coder import JWT_encode, JWT_decode
+from jwt_coder import jwt_encode, jwt_decode
 #JWT_encode takes data and secret and returns the token
 #JWT_decode takes token and returns decoded data
 app = FastAPI()
