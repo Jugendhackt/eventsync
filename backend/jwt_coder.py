865c6aba49d9743e95c82d8102bf6e368b6a67cd
Merlin <70809264+smyril42@users.noreply.github.com>
2024-10-05 16:06:47 +0200

    refactor jwt_coder.py

--- backend/jwt_coder.py
+++ backend/jwt_coder.py
@@ -2,22 +2,25 @@ import jwt
 from jwt.exceptions import ExpiredSignatureError
 
 
-data = { "pw": "1234" }
-SECRET = 'super_secure'
+data = {"pw": "1234"}
+SECRET = "super_secure"
+
 
 def jwt_encode(_data, _secret):
     tokentemp = jwt.encode(payload=_data, key=_secret)
     return tokentemp
 
-TOKEN = jwt_encode(data, SECRET)
-print(TOKEN)
 
-def jwt_decode(_token):
+TOKEN = jwt_encode(data, SECRET)
+
+
+def jwt_decode(token):
     try:
-        _decoded = jwt.decode(_token, key='super_secure', algorithms=['HS256', ])
+        decoded = jwt.decode(token, key='super_secure', algorithms=['HS256', ])
     except ExpiredSignatureError as error:
         print(f'Unable to decode the token, error: {error}')
-    return _decoded
+        raise
+    return decoded
+
 
 decoded = jwt_decode(TOKEN)
-print(decoded)
