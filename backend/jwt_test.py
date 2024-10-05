f575a20c40252014c4f1a54bc06e5ced6a1008b5
Julian <julian@hufmann.info>
2024-10-05 12:35:47 +0200

    jwt_test added as methods JWT_encode and JWT_decode

--- backend/jwt_test.py
+++ backend/jwt_test.py
@@ -1,21 +1,33 @@
 import jwt
 from jwt.exceptions import ExpiredSignatureError
+
+
 data = {
     "pw": "1234",
 }
-
 secret = 'super_secure'
 
-token = jwt.encode(
-    payload=data,
-    key=secret
-)
+def JWT_encode(data, secret):
 
-print(token)
-try:
-    decoded = jwt.decode(token, key='super_secure', algorithms=['HS256', ])
+    tokentemp = jwt.encode(
+        payload=data,
+        key=secret
+    )
+    return(tokentemp)
 
-except ExpiredSignatureError as error:
-    print(f'Unable to decode the token, error: {error}')
+
     
-print(decoded)
+
+token = JWT_encode(data, secret)
+print (token)
+
+def JWT_decode(token, secret):
+    try:
+        decoded = jwt.decode(token, key='super_secure', algorithms=['HS256', ])
+
+    except ExpiredSignatureError as error:
+        print(f'Unable to decode the token, error: {error}')
+    return(decoded)
+        
+decoded = JWT_decode(token, secret)
+print (decoded)
